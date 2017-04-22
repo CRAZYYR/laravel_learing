02bc84a9c81ab4260955f80849f1c2d941f12631
扯嘴 <zscool@foxmail.com>
2017-04-22 16:11:43 +0800

    base has leared by me
    基础的所有都已经了解和掌握   ====笔记===

--- app/Http/Controllers/DBController.php
+++ app/Http/Controllers/DBController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Support\Facades\DB;
+use App\Http\Model\User;
 class DBController extends Controller
 {
     /**
@@ -16,8 +17,32 @@ class DBController extends Controller
      */
     public function index()
     {
-        dd(config('database.connections'));
+       // dd(config('database.connections'));
+       // $pdo=DB::connection()->getPdo();
+       // dd($pdo);
+       // $users=DB::table('user')->where(array('name'=>'admin'))->get();
+       // dd($users);
+       // return view('db');
+       // 
+       // $user = User::where(array('name'=>'admin'));
+       // 
+       // 模型数据的修改
+    //    $user=User::find(1);
+
+    //    $user->name='shuaizi';
+    // $user->update();
+    // 单纯数据修改
+    // 
+    // 
+    // 数据的添加
+    $user=DB::table('user')->insert(array('name'=>'haha','pw'=>123456));
+    // 
+    // 
+    // 数据的删除
+     // $user=DB::table('user')->where('user_id', '>',3)->delete();
+    dd($user);
     }
+
 
     /**
      * Show the form for creating a new resource.
---
+++ resources/views/db.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DB</title>
+</head>
+<body>
+				这是数据库的练习使用！
+</body>
+
+</html>
